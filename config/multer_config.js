2bbafaf9ff5f7bb4bcffb39695b569dd4b3bfd63
nikitadhar <nikitadhar142017@gmail.com>
2026-06-06 19:14:40 +0530

    changing uploads

--- config/multer_config.js
+++ config/multer_config.js
@@ -1,18 +1,7 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/");
-  },
+const storage = multer.memoryStorage();
 
-  filename(req, file, cb) {
-    cb(
-      null,
-      Date.now() + "-" + file.originalname
-    );
-  },
-});
-
-export  const upload = multer({
+export const upload = multer({
   storage,
 });
